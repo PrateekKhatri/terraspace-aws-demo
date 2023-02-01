6cb54dd6fc2681d22a341a4286c590a80c7667a0
PrateekKhatri <70568716+PrateekKhatri@users.noreply.github.com>
2023-02-01 17:39:09 +0530

    Update main.tf

--- app/modules/s3-bucket/main.tf
+++ app/modules/s3-bucket/main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = var.acl
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = var.acl
+
+  versioning {
+    enabled = true
+  }
+}
