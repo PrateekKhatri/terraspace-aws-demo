53e8ae90e65b8f58a570823b82620dfe18ad1d37
PrateekKhatri <khatriprateek077@gmail.com>
2023-02-01 20:24:08 +0530

    test

--- app/stacks/dev/s3_bucket.tf
+++ app/stacks/dev/s3_bucket.tf
@@ -1,9 +1,4 @@
-resource "aws_s3_bucket" "main" {
-  bucket = local.global_csi
-  acl    = var.s3_acl
-
-
-  tags   = merge({
-    Name = local.global_csi
-  },local.default_tags)
+module "s3" {
+  source = "../../modules/s3-bucket"
+  s3_acl = var.s3_acl
 }
